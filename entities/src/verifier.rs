52baef44151d7574d5f713961f4b2d36e83a3f85
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-19 16:03:38 +0200

    Add tests and comments

--- entities/src/verifier.rs
+++ entities/src/verifier.rs
@@ -11,29 +11,99 @@ pub struct Verifier {
 
 
 impl Verifier {
-    pub fn new(params: &ProofParamsPublic) -> Self {
+
+    /// Creates a new `Verifier` instance, associated with the input proof parameters `params`.
+    pub fn new(params: ProofParamsPublic) -> Self {
         Self {
-            params: *params
+            params
         }
     }
 
-    pub fn update_acc(&mut self, acc: &Accumulator) -> Accumulator{
+    /// Updates stored proof params incorporating new accumulator value `acc`.
+    pub fn update_acc(&mut self, acc: Accumulator) -> Accumulator{
         let old_acc = self.params.c_m;
         self.params.c_m = acc.0;
         return Accumulator(old_acc);
     }   
 
-    pub fn update_params(&mut self, params: &ProofParamsPublic)->ProofParamsPublic{
+    /// Updates stored proof params with input parameters `params`.
+    pub fn update_params(&mut self, params: ProofParamsPublic)->ProofParamsPublic{
         let old_params = self.params;
-        self.params = *params;
+        self.params = params;
         return old_params;
     }   
     
+    /// Verifies the input membership proof `mem_proof` against the stored proof parameters.
     pub fn verify(&self, mem_proof: Proof)->bool{
         let mut transcript = Transcript::new(proof::PROOF_LABEL);
         self.params.add_to_transcript(&mut transcript);
 
         let final_proof = mem_proof.finalize(&self.params);
         return final_proof.verify(&mut transcript);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Holder, Issuer, Verifier};
+    use std::time::Instant; 
+
+
+    #[test]
+    fn verifier_proof_succeed() {
+        let mut issuer = Issuer::new(None);
+
+        // Init Holder
+        let rh = issuer.add("holder1").unwrap();
+        let params = issuer.get_proof_params();
+        let holder = Holder::new("holder1", rh, Some(params));
+        
+        // Init Verifier
+        let ver = Verifier::new(params);
+        
+        // Compute proof
+        let proof = holder.proof_membership(&params);
+        
+        // Verify proof
+        let t = Instant::now();
+        assert!(ver.verify(proof));
+        let t = t.elapsed();
+        println!(
+            "Valid proof - verification time: {:?}",
+            t
+        )
+    }
+
+    #[test]
+    fn verifier_proof_fails() {
+        let mut issuer = Issuer::new(None);
+
+        // Init Holder
+        let rh = issuer.add("holder1").unwrap();
+        let params = issuer.get_proof_params();
+        let holder = Holder::new("holder1", rh, Some(params));
+        
+        // Init Verifier
+        let mut ver = Verifier::new(params);
+
+
+        // Delete holder
+        issuer.delete(&String::from("holder1"));
+
+        // Update verifier
+        let new_acc = issuer.get_accumulator();
+        ver.update_acc(new_acc);
+
+        // Compute proof
+        let proof = holder.proof_membership(&params);
+        
+        // Verify proof is not valid
+        let t = Instant::now();
+        assert!(!ver.verify(proof));
+        let t = t.elapsed();
+        println!(
+            "Non valid proof - verification time: {:?}",
+            t
+        )
     }
 }
