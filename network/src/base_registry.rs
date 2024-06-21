594a0cb0b8909fd3473dfbf2c10d736c1a14e131
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-21 15:45:53 +0200

    Add support for update polys and cleanup code

--- network/src/base_registry.rs
+++ network/src/base_registry.rs
@@ -1,21 +1,23 @@
-use accumulator::{
-    proof::ProofParamsPublic, MembershipWitness 
-};
-use entities::issuer::UpdatePolynomials;
-use bls12_381_plus::Scalar;
+use crate::server::Update;
+use accumulator::{proof::ProofParamsPublic, MembershipWitness};
 use axum::{
     body::Bytes,
     extract::{Query, State},
     http::StatusCode,
-    response::IntoResponse,
+    response::{IntoResponse, Response},
     routing::{get, put},
     Router,
 };
-use std::{
-    collections::{BTreeMap, HashMap}, fmt::Debug, sync::{Arc, Mutex}
-};
+use bls12_381_plus::Scalar;
+use entities::issuer::UpdatePolynomials;
 use serde::Deserialize;
-use crate::server::Update;
+use std::{
+    collections::{BTreeMap, HashMap},
+    fmt::Debug,
+    ops::Bound::{Excluded, Included},
+    sync::{Arc, Mutex},
+};
+use tokio::time::Instant;
 
 pub const BASE_REGISTRY_FRONT: &str = "127.0.0.1:3000";
 pub const PARAMS_URL: &str = "/parameteres/proof-params";
@@ -36,181 +38,219 @@ struct AppState {
     wit: Arc<Mutex<HashMap<String, MembershipWitness>>>,
     current_acc_version: Arc<Mutex<usize>>,
     id_to_usize: Arc<Mutex<HashMap<Scalar, usize>>>,
-    update_polys: Arc<Mutex<BTreeMap<usize, Vec<UpdatePolynomials>>>>,
+    update_polys: Arc<Mutex<BTreeMap<usize, UpdatePolynomials>>>,
 }
 
-
-async fn upd_public_params(
-    State(state): State<AppState>,
-    payload: Bytes,
-) -> impl IntoResponse {
+/// Updates the state public parameters with public parameters in the request payload
+async fn replace_public_params(State(state): State<AppState>, payload: Bytes) -> Response {
     match bincode::deserialize::<ProofParamsPublic>(&payload.to_owned()) {
         Ok(pp) => {
-            println!("Public Params Received");
-            
+            println!(
+                "[{:?}] Base Registry: Public Params Received",
+                std::time::SystemTime::now()
+            );
+
+            // Replace public parameters
             state.pp.lock().unwrap().replace(pp);
-                        
+
+            // Increase accumulator version
             let mut acc_version = state.current_acc_version.lock().unwrap();
             *acc_version += 1;
+            // Map new version to new accumulator id
             let mut id_to_usize = state.id_to_usize.lock().unwrap();
             id_to_usize.insert(pp.get_accumulator().get_id(), acc_version.clone());
 
-            return (StatusCode::CREATED, "Ok".to_string());
+            return StatusCode::CREATED.into_response();
         }
-        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid payload".to_string()),
+        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid payload").into_response(),
     }
 }
 
-async fn upd_update_polys(
-    State(state): State<AppState>,
-    payload: Bytes,
-) -> impl IntoResponse {
+/// Uses the revocation updates contained in the request payload 
+/// to update the accumulator state and store new update polynomials
+async fn update_after_revocation(State(state): State<AppState>, payload: Bytes) -> Response {
     match bincode::deserialize::<Update>(&payload.to_owned()) {
         Ok(update) => {
-            println!("Upd Poly Received");
-            
+            println!("Base Registry: upd poly received");
+
+            // Get inputs
             let acc = update.0;
             let polys = update.1;
-
             let acc_id = acc.get_id();
-            state.pp.lock().unwrap().unwrap().update_accumulator(acc);
-            
+
+            // Update public params
+            let mut pp = state.pp.lock().unwrap().unwrap();
+            pp.update_accumulator(acc);
+            state.pp.lock().unwrap().replace(pp);
+
+            // Update accumulator version
             let mut state_version = state.current_acc_version.lock().unwrap();
             *state_version += 1;
             let acc_version = state_version.clone();
             drop(state_version);
 
-            state.id_to_usize.lock().unwrap().insert(acc_id, acc_version);
-            state.update_polys.lock().unwrap().insert(acc_version, polys);
-
-            return (StatusCode::CREATED, "Ok".to_string());
+            // Update state maps
+            state
+                .id_to_usize
+                .lock()
+                .unwrap()
+                .insert(acc_id, acc_version);
+            state
+                .update_polys
+                .lock()
+                .unwrap()
+                .insert(acc_version, polys);
+            return StatusCode::CREATED.into_response();
         }
-        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid payload".to_string()),
+        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid payload").into_response(),
     }
 }
 
-
-async fn get_update_polys(
-    State(state): State<AppState>,
-    payload: Bytes,
-) -> impl IntoResponse {
+/// Returns all the update polynomials needed to get up-to-date starting from the accumulator id 
+/// that is passed in the payload
+async fn get_update_polys(State(state): State<AppState>, payload: Bytes) -> Response {
     println!("Base Registry: get_update_polys called");
 
     match bincode::deserialize::<Scalar>(&payload.to_owned()) {
         Ok(acc_id) => {
-            match state.id_to_usize.lock().unwrap().get(&acc_id){
+            match state.id_to_usize.lock().unwrap().get(&acc_id) {
                 Some(id) => {
-                    let update_polys = state.update_polys.lock().unwrap();
-                    let upd_poly = update_polys.get(id).unwrap();
-                    let payload = serde_json::to_string(upd_poly).unwrap();
-                    return (StatusCode::CREATED, payload);
+                    // Fill vector of updates from input accumulator id
+                    let mut poly_vec = Vec::new();
+                    state
+                        .update_polys
+                        .lock()
+                        .unwrap()
+                        .range(id + 1..)
+                        .for_each(|(_, poly)| poly_vec.push(poly.clone()));
+
+                    // Check if some updates are available
+                    if poly_vec.is_empty() {
+                        return (StatusCode::NOT_FOUND, "No updates available").into_response();
+                    }
+
+                    // Return updates
+                    let payload = bincode::serialize(&poly_vec).unwrap();
+                    return (StatusCode::OK, payload).into_response();
                 }
-                None => {
-                    return (StatusCode::BAD_REQUEST, "Invalid accumulator id".to_string())
-                }
+                None => return (StatusCode::BAD_REQUEST, "Invalid accumulator id").into_response(),
             }
         }
-        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid payload".to_string()),
-    }
-}
-
-
-async fn upd_witnesses(
-    State(state): State<AppState>,
-    body: Bytes,
-) -> impl IntoResponse {
-    match bincode::deserialize::<HashMap<String,MembershipWitness>>(&body.to_owned()){
-        Ok(map) => {
-            println!("New Witnesses Received");
-            let mut wit = state.wit.lock().unwrap();
-            wit.clear();
-            wit.extend(map.into_iter());
-            return (StatusCode::OK, "ok");
-        },
-        Err(e) => {
-            eprintln!("Deserialization error: {}", e);
-            (StatusCode::INTERNAL_SERVER_ERROR, "Deserialization error")
+        Err(_) => {
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Cannot parse payload").into_response()
         }
     }
 }
 
-async fn get_proof_params(State(state): State<AppState>) -> impl IntoResponse {
-    println!("Base Registry: get_proof_params called");
-    match state.pp.lock().unwrap().to_owned() {
-        Some(pp) => return (StatusCode::OK, serde_json::to_string(&pp).unwrap()),
-        None => return (StatusCode::NOT_FOUND, "Paremeters not found".to_string()),
+/// Updates the list of witnesses in the state with those included in the payload
+async fn upd_witnesses(State(state): State<AppState>, body: Bytes) -> Response {
+    match bincode::deserialize::<HashMap<String, MembershipWitness>>(&body.to_owned()) {
+        Ok(map) => {
+            println!("New Witnesses Received");
+
+            // Create new updated witnesses
+            {
+                let mut wit = state.wit.lock().unwrap();
+                wit.clear();
+                wit.extend(map.into_iter());
+            }
+
+            // Clean update info as we have up-to-date witnesses
+            {
+                state.update_polys.lock().unwrap().clear();
+                state.id_to_usize.lock().unwrap().clear();
+                *state.current_acc_version.lock().unwrap() = 0;
+            }
+            return StatusCode::OK.into_response();
+        }
+        Err(e) => {
+            eprintln!("Deserialization error: {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR, "Deserialization error").into_response()
+        }
     }
 }
 
-async fn get_accumulator(State(state): State<AppState>) -> impl IntoResponse {
+
+async fn get_proof_params(State(state): State<AppState>) -> Response {
+    println!("Base Registry: get_proof_params called");
+    match state.pp.lock().unwrap().to_owned() {
+        Some(pp) => {
+            println!("Acc when called: {:?}", pp.get_accumulator());
+            return (StatusCode::OK, bincode::serialize(&pp).unwrap()).into_response();
+        }
+        None => return (StatusCode::NOT_FOUND, "Paremeters not found").into_response(),
+    }
+}
+
+async fn get_accumulator(State(state): State<AppState>) -> Response {
     println!("Base Registry: get_accumulator");
     match state.pp.lock().unwrap().to_owned() {
         Some(pp) => {
+            println!("Acc when called: {:?}", pp.get_accumulator());
             return (
                 StatusCode::OK,
-                serde_json::to_string(&pp.get_accumulator()).unwrap(),
-            )
+                bincode::serialize(&pp.get_accumulator()).unwrap(),
+            ).into_response();
         }
-        None => return (StatusCode::NOT_FOUND, "Paremeters not found".to_string()),
+        None => return (StatusCode::NOT_FOUND, "Accumulator not found").into_response(),
     }
 }
 
-async fn get_public_key(State(state): State<AppState>) -> impl IntoResponse {
+async fn get_public_key(State(state): State<AppState>) -> Response {
     println!("Base Registry: get_public_key called");
     match state.pp.lock().unwrap().to_owned() {
         Some(pp) => {
             return (
                 StatusCode::OK,
-                serde_json::to_string(&pp.get_public_key()).unwrap(),
-            )
+                bincode::serialize(&pp.get_public_key()).unwrap(),
+            ).into_response()
         }
-        None => return (StatusCode::NOT_FOUND, "Paremeters not found".to_string()),
+        None => return (StatusCode::NOT_FOUND, "Public Key not found").into_response(),
     }
 }
 
-async fn get_update(State(state): State<AppState>, query: Query<ExampleParams>)->impl IntoResponse{
+/// Returns the most up-to-date witness for the holder's pseudonym passed in the payload
+async fn get_wit_update(
+    State(state): State<AppState>,
+    query: Query<ExampleParams>,
+) -> Response {
     println!("Base Registry: get_update called");
-    let ret : Vec<u8> = Vec::new();
-    match state.wit.lock().expect("Poisoned mutex").get(&query.pseudo){
-        Some(wit) => {
-            match bincode::serialize(wit) {
-                Ok(bytes) => (StatusCode::OK, bytes),
-                Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, ret),
-            }
+    let ret: Vec<u8> = Vec::new();
+    match state.wit.lock().expect("Poisoned mutex").get(&query.pseudo) {
+        Some(wit) => match bincode::serialize(wit) {
+            Ok(bytes) => (StatusCode::OK, bytes).into_response(),
+            Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, ret).into_response(),
         },
-        None => {
-            (StatusCode::UNAUTHORIZED, ret)
-        }
+        None => (StatusCode::UNAUTHORIZED, ret).into_response(),
     }
-
 }
 
 #[tokio::main]
 pub async fn run() {
-
     let shared_state = AppState {
         pp: Arc::new(Mutex::new(None)),
         wit: Arc::new(Mutex::new(HashMap::new())),
         current_acc_version: Arc::new(Mutex::new(0)),
-        id_to_usize:  Arc::new(Mutex::new(HashMap::new())),
-        update_polys: Arc::new(Mutex::new(BTreeMap::new()))
+        id_to_usize: Arc::new(Mutex::new(HashMap::new())),
+        update_polys: Arc::new(Mutex::new(BTreeMap::new())),
     };
 
     let app = Router::new()
-        .route(PARAMS_URL, get(get_proof_params).put(upd_public_params))
+        .route(PARAMS_URL, get(get_proof_params).put(replace_public_params))
         .with_state(shared_state.clone())
-        .route(POLYS_URL, put(upd_update_polys).get(get_update_polys))
+        .route(POLYS_URL, put(update_after_revocation).get(get_update_polys))
         .with_state(shared_state.clone())
-        .route(WIT_URL, get(get_update).put(upd_witnesses))
+        .route(WIT_URL, get(get_wit_update).put(upd_witnesses))
         .with_state(shared_state.clone())
-        .route(ACC_URL, get(get_accumulator)
-        .with_state(shared_state.clone()))
+        .route(
+            ACC_URL,
+            get(get_accumulator).with_state(shared_state.clone()),
+        )
         .route(PUBKEY_URL, get(get_public_key))
         .with_state(shared_state.clone());
 
-    
-    let listener = tokio::net::TcpListener::bind(BASE_REGISTRY_BACK).await.unwrap();   
+    let listener = tokio::net::TcpListener::bind(BASE_REGISTRY_BACK)
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
-
-
 }
