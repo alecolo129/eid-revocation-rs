07e1ecafaa34afef59a508d1dea6e672f7c507e7
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-24 14:50:28 +0200

    Add macros for logging

---
+++ network/src/macros.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! log_with_time {
+    ($($arg:tt)*) => {{
+        println!("\x1b[32m[{}]\x1b[0m \x1b[36m{}\x1b[0m", 
+        $crate::chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string(), 
+        format!($($arg)*));
+    }};
+}
+
+#[macro_export]
+macro_rules! log_with_time_ln {
+    ($($arg:tt)*) => {{
+        println!("\x1b[32m[{}]\x1b[0m \x1b[36m{}\x1b[0m\n", 
+        $crate::chrono::Local::now().format("%Y-%m-%d %H:%M:%S%.3f").to_string(), 
+        format!($($arg)*));
+    }};
+}
+
