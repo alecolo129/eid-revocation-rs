7ffc32bc20799d710cf257cd84f0a2c6b28371d9
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-24 08:26:16 +0200

    Add support for polynomial update

--- network/src/client.rs
+++ network/src/client.rs
@@ -1,29 +1,69 @@
 use crate::{
-    base_registry::{BASE_REGISTRY_FRONT, PARAMS_URL, WIT_URL},
-    server::{ISSUE_URL, WEBSERVER},
+    base_registry::{BASE_REGISTRY_FRONT, PARAMS_URL, POLYS_URL, WIT_URL},
+    server::{Update, ISSUE_URL, WEBSERVER},
 };
 use accumulator::{
     proof::ProofParamsPublic, witness::MembershipWitness
 };
 
 use entities::{
-    holder::Holder, issuer::RevocationHandle, 
+    holder::Holder, issuer::RevocationHandle, UpdatePolynomials, 
 };
 
 pub struct Client;
 
 impl Client {
 
+    /// Ask the issuer to add new holder associated with psuedonym `pseudo` and return respective revocation handle
+    pub async fn ask_issuance(pseudo: String) -> Result<RevocationHandle, Box<dyn std::error::Error>> {
+        let url = format!("http://{WEBSERVER}{ISSUE_URL}");
+        let resp = reqwest::Client::new()
+            .post(url)
+            .body(pseudo.clone())
+            .send()
+            .await?
+            .error_for_status()?;
+        let rh: RevocationHandle = bincode::deserialize(&resp.bytes().await?)?;
+        Ok(rh)
+    }
+
+    /// Get proof public parameters from the Base Registry
     pub async fn ask_pp() -> Result<ProofParamsPublic, Box<dyn std::error::Error>> {
         let url = format!("http://{BASE_REGISTRY_FRONT}{PARAMS_URL}");
         let req = reqwest::Client::new().get(url);
-
-        let body = req.send().await?.text().await?;
-        let pp: ProofParamsPublic = serde_json::from_str(&body)?;
+        let body = req.send().await?.bytes().await?;
+        let pp: ProofParamsPublic = bincode::deserialize(&body)?;
         return Ok(pp);
     }
 
-    pub async fn ask_update(hol: &mut Holder) -> Result<bool, Box<dyn std::error::Error>> {
+    /// Get proof public parameters from the Base Registry
+    pub async fn ask_accumulator() -> Result<ProofParamsPublic, Box<dyn std::error::Error>> {
+        let url = format!("http://{BASE_REGISTRY_FRONT}{PARAMS_URL}");
+        let req = reqwest::Client::new().get(url);
+        let body = req.send().await?.bytes().await?;
+        let pp: ProofParamsPublic = bincode::deserialize(&body)?;
+        return Ok(pp);
+    }
+
+    /// Update witness of the input `Holder` instance `holder`. 
+    /// It uses `holder`'s last accumulator id to query the Base Register 
+    /// and get the respective update polynomials.
+    pub async fn poly_upd(holder: &mut Holder) -> Result<bool, Box<dyn std::error::Error>> {
+        let url = format!("http://{BASE_REGISTRY_FRONT}{POLYS_URL}");
+
+        // Get last valid accumulator id and ask update poly
+        let acc_id = holder.get_accumulator_id();
+        let body = bincode::serialize(&acc_id)?;
+        let req = reqwest::Client::new().get(url).body(body);
+        let resp = req.send().await?.error_for_status()?;
+
+        let body = resp.bytes().await?;
+        let updates: Vec<UpdatePolynomials> = bincode::deserialize(&body)?;
+        holder.batch_updates(updates.as_slice())?;
+        return Ok(true);
+    }
+
+    pub async fn wit_upd(hol: &mut Holder) -> Result<bool, Box<dyn std::error::Error>> {
         let url = format!("http://{BASE_REGISTRY_FRONT}{WIT_URL}");
         let req = reqwest::Client::new()
             .get(url)
@@ -42,16 +82,5 @@ impl Client {
         }
     }
 
-    pub async fn ask_issuance(pseudo: String) -> Result<RevocationHandle, Box<dyn std::error::Error>> {
-        let url = format!("http://{WEBSERVER}{ISSUE_URL}");
-        let resp = reqwest::Client::new()
-            .post(url)
-            .body(pseudo.clone())
-            .send()
-            .await?;
-        let rh: RevocationHandle = bincode::deserialize(&resp.bytes().await?)?;
-        //self.holder = Holder::new(rh.get_elem(), self.holder.pp, Some(rh.get_witness()));
-        Ok(rh)
-    }
 
 }
