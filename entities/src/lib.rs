a597a751c7e34d98c1784307943631be4d441ac8
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-24 15:09:05 +0200

    Add trait

--- entities/src/lib.rs
+++ entities/src/lib.rs
@@ -6,3 +6,10 @@ pub mod verifier;
 pub use holder::*;
 pub use verifier::*;
 pub use issuer::*;
+
+
+/// Client trait for updating public parameters
+pub trait Updatable {
+    fn update_accumulator(&mut self, acc: accumulator::Accumulator);
+    fn update_public_params(&mut self, new_pp: accumulator::ProofParamsPublic);
+}
