3028e0a05de3f5d933828477303158ff881ba5d6
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-06-24 15:05:35 +0200

    Clean code and add support for automatically sending witnesses after update

--- network/src/controller.rs
+++ network/src/controller.rs
@@ -20,60 +20,85 @@ impl Controller{
         return resp;
     }
 
+    /// Ask server to add a pseudnym `pseudo` to the revocation batch. 
+    /// 
+    /// NOTE: the respective holder is not instantly revoked. Revocation will be enforced as
+    /// soon as the `Controller::update` function is called.
     pub async fn revoke(pseudo: &str)->Result<Response, Error>{
         let url = format!("http://{WEBSERVER}{REVOKE_URL}");
         let req = reqwest::Client::new().delete(url).body(pseudo.to_string());
         return Self::poll(&req).await;         
     }
 
-    pub async fn revoke_batch(pseudos: Vec<String>)->Result<Response, Error>{
+    /// Ask server to add a list of pseudnyms `pseudos` to the revocation batch. 
+    /// 
+    /// NOTE: the respective holders are not instantly revoked. Revocation will be enforced as
+    /// soon as the `Controller::update` function is called.
+    pub async fn revoke_batch(pseudos: &Vec<String>)->Result<Response, Error>{
         let url = format!("http://{WEBSERVER}{REVOKE_BATCH_URL}");
         let body = bincode::serialize(&pseudos).expect("Error while serializing list of revoked pseudos"); 
         let req = reqwest::Client::new().delete(url).body(body);
         return Self::poll(&req).await;         
     }
 
+    /// Ask server to instantly revoke the holder associated with pseudonym `pseudo`.
+    ///
+    /// NOTE: the computed update polynomials and the updated accumulator value
+    /// are NOT automatically sent to the BaseRegistry.
     pub async fn revoke_now(pseudo: &str)->Result<Response, Error>{
         /*TODO: Add endpoint in Server.rs to do this with one query*/
-        let resp = Self::revoke(pseudo).await?;
-        if !resp.status().is_success(){
-            return Ok(resp);
-        }
+        Self::revoke(pseudo).await?.error_for_status()?;
         Self::update().await
     }
 
-    pub async fn revoke_batch_now(pseudos: Vec<String>)->Result<Response, Error>{
+    /// Ask server to instantly revoke a list of holders associated with pseudonyms `pseudos`.
+    /// 
+    /// NOTE: the computed update polynomials and the updated accumulator value
+    /// are NOT automatically sent to the BaseRegistry.
+    pub async fn revoke_batch_now(pseudos: &Vec<String>)->Result<Response, Error>{
         /*TODO: Add endpoint in Server.rs to do this with one query*/
-        let resp = Self::revoke_batch(pseudos).await?;
-        if !resp.status().is_success(){
-            return Ok(resp);
-        }
+        Self::revoke_batch(pseudos).await?.error_for_status()?;
         Self::update().await
     }
 
-    pub async fn send_public_params()->Result<Response, Error>{
-        let url = format!("http://{WEBSERVER}{SEND_PARAMS_URL}");
-        let req = reqwest::Client::new().get(url);        
-        return Self::poll(&req).await;
-    }
-
-    pub async fn send_witnesses()->Result<Response, Error>{
-        let url = format!("http://{WEBSERVER}{SEND_WIT_URL}");
-        let req = reqwest::Client::new().get(url);        
-        return Self::poll(&req).await;
-    }
-
+    /// Ask server to compute update polynomials for all holders in the revocation batch.
     pub async fn update()->Result<Response, Error>{
         let url = format!("http://{WEBSERVER}{UPD_URL}");
         let req = reqwest::Client::new().put(url);
         return Self::poll(&req).await;
     }
 
+    /// Ask server to compute periodic witness update for all non-revoked holders.
     pub async fn update_periodic()->Result<Response,Error>{
         let url = format!("http://{WEBSERVER}{UPD_PERIODIC_URL}");
         let req = reqwest::Client::new().put(url);        
         return Self::poll(&req).await;
     }
 
+    /// Ask server to compute periodic witness update for all non-revoked holders.
+    /// 
+    /// After the update, sends new witnesses and public parameters to the Base Registry.
+    pub async fn update_periodic_and_send()->Result<(), Box<dyn std::error::Error>>{
+        /*TODO: Add endpoint in Server.rs to do this with one query*/
+        Self::update_periodic().await?.error_for_status()?;
+        Self::send_witnesses().await?.error_for_status()?;
+        Self::send_public_params().await?.error_for_status()?;
+        Ok(())
+    }
+
+    /// Ask server to send up-to-date list of witnesses to the Base Registry.
+    pub async fn send_witnesses()->Result<Response, Error>{
+        let url = format!("http://{WEBSERVER}{SEND_WIT_URL}");
+        let req = reqwest::Client::new().get(url);        
+        return Self::poll(&req).await;
+    }
+
+
+    /// Ask server to send up-to-date public parameters to the Base Registry.
+    pub async fn send_public_params()->Result<Response, Error>{
+        let url = format!("http://{WEBSERVER}{SEND_PARAMS_URL}");
+        let req = reqwest::Client::new().get(url);        
+        return Self::poll(&req).await;
+    }
     
 }
