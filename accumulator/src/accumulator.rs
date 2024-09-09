a42ebde6cdbe91f4e5fe5cf64a0414e3855353d1
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-09-09 19:19:33 +0200

    Clean-up code

--- accumulator/src/accumulator.rs
+++ accumulator/src/accumulator.rs
@@ -17,35 +17,6 @@ use std::hash::{Hash, Hasher};
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Element(pub Scalar);
 
-impl Hash for Element {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.0.to_be_bytes().hash(state)
-    }
-}
-
-impl fmt::Display for Element {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "Element {{ {} }}", self.0)
-    }
-}
-
-
-impl TryFrom<&[u8; 32]> for Element {
-    type Error = Error;
-
-    fn try_from(value: &[u8; 32]) -> Result<Self, Self::Error> {
-        let s = Scalar::from_be_bytes(value);
-        if s.is_some().unwrap_u8() == 1u8 {
-            Ok(Self(s.unwrap()))
-        } else {
-            Err(Error {
-                message: String::from("incorrect byte sequence"),
-                code: 1,
-            })
-        }
-    }
-}
-
 impl Element {
     pub const BYTES: usize = 32;
     
@@ -80,9 +51,48 @@ impl Element {
 }
 
 
+impl Hash for Element {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.0.to_be_bytes().hash(state)
+    }
+}
+
+impl fmt::Display for Element {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "Element {{ {} }}", self.0)
+    }
+}
+
+
+impl TryFrom<&[u8; 32]> for Element {
+    type Error = Error;
+
+    fn try_from(value: &[u8; 32]) -> Result<Self, Self::Error> {
+        let s = Scalar::from_be_bytes(value);
+        if s.is_some().unwrap_u8() == 1u8 {
+            Ok(Self(s.unwrap()))
+        } else {
+            Err(Error {
+                message: String::from("incorrect byte sequence"),
+                code: 1,
+            })
+        }
+    }
+}
+
 /// A coefficent for updating witnesses
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Coefficient(pub G1Projective);
+impl Coefficient {
+    const BYTES: usize = 48;
+
+    /// The byte representation of this coefficient
+    pub fn to_bytes(&self) -> [u8; Self::BYTES] {
+        let mut d = [0u8; Self::BYTES];
+        d.copy_from_slice(self.0.to_bytes().as_ref());
+        d
+    }
+}
 
 impl fmt::Display for Coefficient {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -118,21 +128,111 @@ impl TryFrom<&[u8; 48]> for Coefficient {
     }
 }
 
-impl Coefficient {
-    const BYTES: usize = 48;
 
-    /// The byte representation of this coefficient
+/// Represents a Positive Bilinear Accumulator.
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Accumulator(pub G1Projective);
+
+impl Accumulator {
+    pub const BYTES: usize = 48;
+
+    /// Creates a new random accumulator 
+    /// as in https://ieeexplore.ieee.org/abstract/document/9505229 Section IV.
+    pub fn random(rng: impl RngCore + CryptoRng) -> Self {
+        let s = generate_fr(SALT, None, rng);
+        Self(G1Projective::GENERATOR * s)
+    }
+
+    /// Using the trapdoor `key`, returns a new accumulator without the input element `deletion`
+    pub fn remove(&self, key: &SecretKey, deletion: Element) -> Accumulator{
+        self.clone().remove_assign(key, deletion)
+    }
+
+    /// Using the trapdoor `key`, modifyies the acumulator in-place, removing the input element `deletion`
+    pub fn remove_assign(&mut self, key: &SecretKey, deletion: Element) -> Accumulator{
+        self.0 *= key.batch_deletions(&[deletion]).0;
+        *self
+    }
+
+    /// Using the trapdoor `key`, returns a new accumulator without the values in `deletions`. 
+    pub fn remove_elements(&self, key: &SecretKey, deletions: &[Element]) -> Accumulator {
+        let mut a = self.clone();
+        a.remove_elements_assign(key, deletions);
+        a
+    }
+
+    /// Using the trapdoor `key`, removes values in `deletions` 
+    /// and updates the accumulator in-place.
+    pub fn remove_elements_assign(&mut self, key: &SecretKey, deletions: &[Element]){
+        // V_{t+1} = V_{t}*((x+e_1)*...*(x+e_n))^-1
+        self.0 *= key.batch_deletions(deletions).0;
+    }
+
+    /// Given the accumulator trapdoor `key` and a list of deletions `deletions`, 
+    /// returns the list of update coefficients for polynomial `Ω(X)` without updating the accumulator.
+    pub fn update(
+        &self,
+        key: &SecretKey,
+        deletions: &[Element]
+    ) -> Vec<Coefficient>{
+        self.clone().update_assign(key, deletions)
+    }
+
+    /// Given the accumulator trapdoor `key` and a list of deletions `deletions`, 
+    /// performs a batch update of the accumulator returning the list of update coefficients.
+    pub fn update_assign(
+        &mut self,
+        key: &SecretKey,
+        deletions: &[Element],
+    ) -> Vec<Coefficient> {
+
+        // See page 34 of my thesis (eq. 4.4, 4.7)
+
+        // d = ((x+e_1)*...*(x+e_m))^-1
+        let d = key.batch_deletions(deletions);
+        // v(X) = ∑^{m-1}_{i=0} c_i X^i
+        let coefficients = key.gen_up_poly(deletions);
+
+        // Optimized evaluation of [c_0*V, ..., c_{m-1}*V] using window multiplication
+        let coefficients = window_mul(self.0, coefficients.into_iter().map(|c| c.0).collect());
+
+        // V_{t+1} = V_t*d
+        self.0 *= d.0;
+
+        coefficients.into_iter().map(|c| Coefficient(c)).collect()
+    }
+
+    /// Generate accumulator id
+    pub fn get_id(&self) -> Scalar{
+        return generate_fr(SALT, Some(&self.to_bytes()), rand_core::OsRng{})
+    }
+
+    /// Convert accumulator to bytes
     pub fn to_bytes(&self) -> [u8; Self::BYTES] {
         let mut d = [0u8; Self::BYTES];
         d.copy_from_slice(self.0.to_bytes().as_ref());
         d
     }
+
+    /// Performs a batch deletion as described on page 11, section 5 in
+    /// https://eprint.iacr.org/2020/777.pdf. Unoptimized version, maintained only for testing
+    fn _update_assign(
+        &mut self,
+        key: &SecretKey,
+        deletions: &[Element],
+    ) -> Vec<Coefficient> {
+        let d = key.batch_deletions(deletions);
+        let coefficients = key
+            .gen_up_poly(deletions)
+            .iter()
+            .map(|c| Coefficient(self.0 * c.0))
+            .collect();
+        self.0 *= d.0;
+        coefficients
+    }
+
 }
 
-
-/// Represents a Positive Bilinear Accumulator.
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
-pub struct Accumulator(pub G1Projective);
 
 impl fmt::Display for Accumulator {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -174,100 +274,6 @@ impl Default for Accumulator {
     }
 }
 
-impl Accumulator {
-    pub const BYTES: usize = 48;
-
-    /// Creates a new random accumulator 
-    /// as in https://ieeexplore.ieee.org/abstract/document/9505229 Section IV.
-    pub fn random(rng: impl RngCore + CryptoRng) -> Self {
-        let s = generate_fr(SALT, None, rng);
-        Self(G1Projective::GENERATOR * s)
-    }
-
-    pub fn remove(&self, key: &SecretKey, deletion: Element) -> Accumulator{
-        self.clone().remove_assign(key, deletion)
-    }
-
-    pub fn remove_assign(&mut self, key: &SecretKey, deletion: Element) -> Accumulator{
-        self.0 *= key.batch_deletions(&[deletion]).0;
-        *self
-    }
-
-    /// Using the trapdoor `key`, returns a new accumulator without the values in `deletions`. 
-    /// Does not compute the update coefficients.
-    pub fn remove_elements(&self, key: &SecretKey, deletions: &[Element]) -> Accumulator {
-        let mut a = self.clone();
-        a.remove_elements_assign(key, deletions);
-        a
-    }
-
-    /// Using the trapdoor `key`, removes values in `deletions` 
-    /// and updates the accumulator, without computing update coefficients.
-    pub fn remove_elements_assign(&mut self, key: &SecretKey, deletions: &[Element]) -> Accumulator{
-        // V' = V*((𝛼+y_1)*...*(𝛼+y_n))^-1
-        self.0 *= key.batch_deletions(deletions).0;
-        *self
-    }
-
-    /// Given the accumulator trapdoor `key` and a list of deletions `deletions`, 
-    /// returns the list of update coefficients without updating the accumulator.
-    /// The coefficients are computed as described on page 11, section 5 in https://eprint.iacr.org/2020/777.pdf. 
-    pub fn update(
-        &self,
-        key: &SecretKey,
-        deletions: &[Element]
-    ) -> Vec<Coefficient>{
-        self.clone().update_assign(key, deletions)
-    }
-
-    /// Given the accumulator trapdoor `key` and a list of deletions `deletions`, 
-    /// performs a batch update of the accumulator returning the list of update coefficients.
-    /// The coefficients are computed as described on page 11, section 5 in https://eprint.iacr.org/2020/777.pdf. 
-    pub fn update_assign(
-        &mut self,
-        key: &SecretKey,
-        deletions: &[Element],
-    ) -> Vec<Coefficient> {
-        let d = key.batch_deletions(deletions);
-        let coefficients = key.create_coefficients(deletions);
-
-        // Optimized evaluation of [c_1*V, ..., c_n*V] using window multiplication
-        let coefficients = window_mul(self.0, coefficients.into_iter().map(|c| c.0).collect());
-
-        // V' = V*((𝛼+y_1)*...*(𝛼+y_n))^-1
-        self.0 *= d.0;
-        coefficients.into_iter().map(|c| Coefficient(c)).collect()
-    }
-
-    /// Performs a batch deletion as described on page 11, section 5 in
-    /// https://eprint.iacr.org/2020/777.pdf. Unoptimized version, maintained only for testing
-    fn _update_assign(
-        &mut self,
-        key: &SecretKey,
-        deletions: &[Element],
-    ) -> Vec<Coefficient> {
-        let d = key.batch_deletions(deletions);
-        let coefficients = key
-            .create_coefficients(deletions)
-            .iter()
-            .map(|c| Coefficient(self.0 * c.0))
-            .collect();
-        self.0 *= d.0;
-        coefficients
-    }
-
-    /// Generate accumulator id
-    pub fn get_id(&self) -> Scalar{
-        return generate_fr(SALT, Some(&self.to_bytes()), rand_core::OsRng{})
-    }
-
-    /// Convert accumulator to bytes
-    pub fn to_bytes(&self) -> [u8; Self::BYTES] {
-        let mut d = [0u8; Self::BYTES];
-        d.copy_from_slice(self.0.to_bytes().as_ref());
-        d
-    }
-}
 
 
 #[cfg(test)]
@@ -277,7 +283,7 @@ mod tests {
     use group::ff::{Field, PrimeField};
     use rand::rngs::OsRng;
     use std::time::SystemTime;
-    use crate::{Deletion, MembershipWitness, PublicKey};
+    use crate::{UpMsg, MembershipWitness, PublicKey};
 
     use super::*;
 
@@ -333,8 +339,8 @@ mod tests {
     #[test]
     fn acc_batch_update_test() {
 
-        const CREDENTIAL_SPACE: usize = 18_000;
-        const BATCH_DELETIONS: usize = 16_000;
+        const CREDENTIAL_SPACE: usize = 1_010;
+        const BATCH_DELETIONS: usize = 1_000;
 
         // Generate params        
         let key = SecretKey::new(Some(b"Key{i}"));
@@ -354,15 +360,15 @@ mod tests {
         let coeff = a.update_assign(&key, revoked);
         let t1 = t1.elapsed();
 
-        /* 
+        
         let t2 = Instant::now();
         let coeff2 = a2._update_assign(&key, revoked);
         let t2 = t2.elapsed();
 
-        assert_eq!(coeff, coeff2);*/
+        assert_eq!(coeff, coeff2);
 
-        //println!("Time to compute {BATCH_DELETIONS} updates: {:?}", t2);
-        println!("Time to compute {BATCH_DELETIONS} updates with windowed multiplication: {:?}", t1);
+        println!("Time to compute update poly omega for {BATCH_DELETIONS} deletions: {:?}", t2);
+        println!("Time to compute updates poly omega for {BATCH_DELETIONS} deletions with windowed multiplication: {:?}", t1);
     }
 
    
