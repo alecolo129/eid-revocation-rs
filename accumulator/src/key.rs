2114008120f152ce055a4ca9533ed57532a469a6
Alessandro Colombo <alessandro.colombo@epfl.ch>
2024-10-21 12:08:46 +0200

    Remove useless _create_coefficient function. Directly use agora implementation for comparison.

--- accumulator/src/key.rs
+++ accumulator/src/key.rs
@@ -94,52 +94,7 @@ impl SecretKey {
         }
         v.0.into_iter().map(|b| Element(b)).collect()
     }
-
-    /// Create the Batch Polynomial coefficients as by Mike Lodder's implementation
-    fn _create_coefficients(&self, additions: &[Element], deletions: &[Element]) -> Vec<Element> {
-        // vD(x) = ∑^{m}_{s=1}{ ∏ 1..s {yD_i + alpha}^-1 ∏ 1 ..s-1 {yD_j - x}
-        let one = Scalar::ONE;
-        let m1 = -one;
-        let mut v_d = Polynomial::with_capacity(deletions.len());
-        for s in 0..deletions.len() {
-            // ∏ 1..s (yD_i + alpha)^-1
-            let c = self.batch_deletions(&deletions[0..s + 1]).0;
-            let mut poly = Polynomial::with_capacity(deletions.len());
-            poly.push(one);
-            // ∏ 1..(s-1) (yD_j - x)
-            for j in deletions.iter().take(s) {
-                poly *= &[j.0, m1];
-            }
-            poly *= c;
-            v_d += poly;
-        }
-
-        //v_d(x) * ∏ 1..n (yA_i + alpha)
-        v_d *= self.batch_additions(additions).0;
-
-        // vA(x) = ∑^n_{s=1}{ ∏ 1..s-1 {yA_i + alpha} ∏ s+1..n {yA_j - x} }
-        let mut v_a = Polynomial::with_capacity(additions.len());
-        for s in 0..additions.len() {
-            // ∏ 1..s-1 {yA_i + alpha}
-            let c = if s == 0 {
-                one
-            } else {
-                self.batch_additions(&additions[0..s]).0
-            };
-            let mut poly = Polynomial::with_capacity(additions.len());
-            poly.push(one);
-            // ∏ s+1..n {yA_j - x}
-            for j in additions.iter().skip(s + 1) {
-                poly *= &[j.0, m1];
-            }
-            poly *= c;
-            v_a += poly;
-        }
-        // vA - vD
-        v_a -= v_d;
-
-        v_a.0.iter().map(|b| Element(*b)).collect()
-    }
+ 
 
     /// Return the raw byte representation of the key
     pub fn to_bytes(&self) -> [u8; Self::BYTES] {
@@ -206,7 +161,7 @@ impl TryFrom<&[u8; 96]> for PublicKey {
 mod tests {
     use super::*;
     use std::time::Instant;
-
+    use agora_allosaurus_rs::accumulator as ago_acc;
     #[test]
     fn key_batch_test() {
         // Init parameters
@@ -245,8 +200,11 @@ mod tests {
         (0..BATCH_SIZE).for_each(|i| data.push(Element::hash(format!("Element {i}").as_bytes())));
 
         // Compute update coefficients with optimization
+        
         let t1 = Instant::now();
-        let coefficients = key._create_coefficients(&[], &data);
+        let ago_key = ago_acc::SecretKey(key.0);
+        let ago_data: Vec<_> = data.iter().map(|d| ago_acc::Element(d.0)).collect();
+        let coefficients = ago_key.create_coefficients(&[], &ago_data);
         let t1 = t1.elapsed();
 
         // Compute update coefficients without optimization
@@ -262,7 +220,7 @@ mod tests {
                 assert_eq!(c_1.0, -c_2.0);
             });
 
-        println!("Old coefficient generation: {:?}", t1);
+        println!("Agora Implementation: coefficient generation: {:?}", t1);
         println!("New coefficient generation: {:?}", t2);
     }
 }
